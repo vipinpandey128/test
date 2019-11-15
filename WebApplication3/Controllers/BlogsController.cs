c87d5744bebafdf6e2247c50133aa433129ac467
vipinp2 <vipinp2@CHETUIWK0372>
2019-11-15 23:11:26 +0530

    new ch

---
+++ WebApplication3/Controllers/BlogsController.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using WebApplication3.DbContexts;
+using WebApplication3.Models;
+
+namespace WebApplication3.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BlogsController : ControllerBase
+    {
+        private readonly DataContext _context;
+
+        public BlogsController(DataContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Blogs
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Blog>>> GetBlogs()
+        {
+            return await _context.Blogs.ToListAsync();
+        }
+
+        // GET: api/Blogs/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Blog>> GetBlog(long id)
+        {
+            var blog = await _context.Blogs.FindAsync(id);
+
+            if (blog == null)
+            {
+                return NotFound();
+            }
+
+            return blog;
+        }
+
+        // PUT: api/Blogs/5
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutBlog(long id, Blog blog)
+        {
+            if (id != blog.BlogId)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(blog).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!BlogExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Blogs
+        [HttpPost]
+        public async Task<ActionResult<Blog>> PostBlog(Blog blog)
+        {
+            _context.Blogs.Add(blog);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetBlog", new { id = blog.BlogId }, blog);
+        }
+
+        // DELETE: api/Blogs/5
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<Blog>> DeleteBlog(long id)
+        {
+            var blog = await _context.Blogs.FindAsync(id);
+            if (blog == null)
+            {
+                return NotFound();
+            }
+
+            _context.Blogs.Remove(blog);
+            await _context.SaveChangesAsync();
+
+            return blog;
+        }
+
+        private bool BlogExists(long id)
+        {
+            return _context.Blogs.Any(e => e.BlogId == id);
+        }
+    }
+}
